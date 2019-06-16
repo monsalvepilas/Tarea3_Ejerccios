a9f3995f2a470fed9be0191982136f5cd389cc0c
monsalvepilas <gsfpdaw@gmail.com>
2019-06-16 21:06:07 +0200

    Update Principal.java
    Giro de la caja completado

--- JavaApplication46/src/caja/Principal.java
+++ JavaApplication46/src/caja/Principal.java
@@ -1,86 +1,91 @@
-
 package caja;
 
 import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// PUNTO 6
+        // PUNTO 6
+        Caja dibujar = new Caja();
 
-		Caja dibujar = new Caja();
+        // PETICIÓN DE DATOS POR TECLADO
+        Scanner Datos_Caja = new Scanner(System.in);
 
-		// PETICIÓN DE DATOS POR TECLADO
+        System.out.print("Introduce el carácter de la caja: ");
 
-		Scanner Datos_Caja = new Scanner(System.in);
+        dibujar.establece_caracter(Datos_Caja.nextLine());
 
-		System.out.print("Introduce el carácter de la caja: ");
+        System.out.print("Introduce el núemro de columnas: ");
 
-		dibujar.establece_caracter(Datos_Caja.nextLine());
+        dibujar.establece_columnas(Datos_Caja.nextInt());
 
-		System.out.print("Introduce el núemro de columnas: ");
+        System.out.print("Introduce el número de filas: ");
 
-		dibujar.establece_columnas(Datos_Caja.nextInt());
+        dibujar.establece_filas(Datos_Caja.nextInt());
 
-		System.out.print("Introduce el número de filas: ");
+        System.out.println("");
+        System.out.println("");
 
-		dibujar.establece_filas(Datos_Caja.nextInt());
+        for (int contador_col = 0; contador_col < dibujar.dime_filas(); contador_col++) {
+            System.out.print(dibujar.dime_caracter());
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+            for (int contador_fil = 1; contador_fil < dibujar.dime_columnas(); contador_fil++) {
+                System.out.print(dibujar.dime_caracter());
+            }
 
-		for (int contador_col = 0; contador_col < dibujar.dime_filas(); contador_col++) {
-			System.out.print(dibujar.dime_caracter());
+            System.out.println("");
 
-			for (int contador_fil = 1; contador_fil < dibujar.dime_columnas(); contador_fil++)
+        }
 
-				System.out.print(dibujar.dime_caracter());
+        System.out.println("");
+        System.out.println("");
 
-			System.out.println("");
+        System.out.print("Introduce el valor para agrandar la anchura de la caja: ");
 
-		}
+        dibujar.aumentarAnchura(Datos_Caja.nextInt());
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
-		System.out.print("Introduce el valor para agrandar la anchura de la caja: ");
+        for (int contador_col = 0; contador_col < dibujar.dime_filas(); contador_col++) {
+            System.out.print(dibujar.dime_caracter());
 
-		dibujar.aumentarAnchura(Datos_Caja.nextInt());
+            for (int contador_fil = 1; contador_fil < dibujar.dime_anchura(); contador_fil++) {
+                System.out.print(dibujar.dime_caracter());
+            }
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+            System.out.println("");
+        }
 
-		for (int contador_col = 0; contador_col < dibujar.dime_filas(); contador_col++) {
-			System.out.print(dibujar.dime_caracter());
+        System.out.println("");
+        System.out.println("");
+        System.out.println("" + dibujar.dime_incremento());
 
-			for (int contador_fil = 1; contador_fil < dibujar.dime_anchura(); contador_fil++)
+        // COMPROBACIONES
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
-				System.out.print(dibujar.dime_caracter());
+        System.out.println("El carácter es: " + dibujar.dime_caracter());
 
-			System.out.println("");
-		}
+        System.out.println("El número de columnas es: " + dibujar.dime_columnas());
 
-                System.out.println("");
-		System.out.println("" + dibujar.dime_incremento());
+        System.out.println("Y el número de filas es: " + dibujar.dime_filas());
 
-		// COMPROBACIONES
+        System.out.println("Y el número de columnas aumentado es: " + dibujar.dime_anchura());
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
-		System.out.println("El carácter es: " + dibujar.dime_caracter());
+        System.out.print("Quieres girar la caja: ");
+        dibujar.giramos_caja(Datos_Caja.nextLine());
+        System.out.print("");
+        dibujar.giramos_caja(Datos_Caja.nextLine());
 
-		System.out.println("El número de columnas es: " + dibujar.dime_columnas());
+        System.out.println("");
+        System.out.println("");
 
-		System.out.println("Y el número de filas es: " + dibujar.dime_filas());
-
-		System.out.println("Y el número de columnas aumentado es: " + dibujar.dime_anchura());
-
-	}
-
+        System.out.println("" + dibujar.girarCaja());
+    }
 }
